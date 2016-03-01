369f607df1efa7cd715a1d5ed4d86b972f44d23b
Mike Putnam <mike@theputnams.net>
2016-03-01 13:32:36 -0600

    Select MAX people randomly, else all.

--- project/home/views.py
+++ project/home/views.py
@@ -5,12 +5,15 @@ from flask import render_template, Blueprint
 from project import db   # pragma: no cover
 from project.models import Person   # pragma: no cover
 
+import random
+
 
 # config
 home_blueprint = Blueprint(
     'home', __name__,
     template_folder='templates'
 )   # pragma: no cover
+MAX_GRID_SIZE_HOMEPAGE_PEOPLE = 6
 
 
 # routes
@@ -18,6 +21,15 @@ home_blueprint = Blueprint(
 @home_blueprint.route('/', methods=['GET', 'POST'])   # pragma: no cover
 def home():
     error = None
-    persons = db.session.query(Person).all()
+    current_person_count = db.session.query(Person).count()
+    if current_person_count <= MAX_GRID_SIZE_HOMEPAGE_PEOPLE:
+        persons = db.session.query(Person).all()
+    else:
+        persons = []
+        while len(persons) < MAX_GRID_SIZE_HOMEPAGE_PEOPLE:
+            rand = random.randrange(0, current_person_count)
+            random_person = db.session.query(Person)[rand]
+            if random_person not in persons:
+                persons.append(random_person)
     return render_template(
         'index.html', persons=persons, error=error)
