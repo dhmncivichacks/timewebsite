9d2658a41c52ce994ab7e7979e437c281c45189d
Mike Putnam <mike@theputnams.net>
2016-03-21 21:12:10 -0500

    Add previously gitignored blog bits

---
+++ project/blog/views.py
@@ -0,0 +1,22 @@
+# imports
+from flask import render_template, Blueprint
+
+from project import db   # pragma: no cover
+from project.models import Blog   # pragma: no cover
+from project.models import BlogPost   # pragma: no cover
+
+
+# config
+blog_blueprint = Blueprint(
+    'blog', __name__,
+    template_folder='templates'
+)   # pragma: no cover
+
+# routes
+# use decorators to link the function to a url
+@blog_blueprint.route('/blog', methods=['GET', 'POST'])   # pragma: no cover
+def blog():
+    error = None
+    posts = db.session.query(BlogPost).all()
+    return render_template(
+        'blog.html', posts=posts, error=error)
