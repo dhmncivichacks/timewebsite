7f4a96127178c13c83c2fd4279eee95003a33b1f
Mike Putnam <mike@theputnams.net>
2016-09-05 13:30:59 -0500

    Just use url_handle for username

--- project/models.py
+++ project/models.py
@@ -133,14 +133,12 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String)
     posts = relationship("BlogPost", backref="author")
     url_handle = db.Column(db.String, nullable=False)
 
-    def __init__(self, name, email, password, url_handle):
-        self.name = name
+    def __init__(self, email, password, url_handle):
         self.email = email
         self.password = bcrypt.generate_password_hash(password)
         self.url_handle = url_handle
